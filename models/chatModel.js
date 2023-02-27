d8454c595bc293ea12c7530cb0dbcbd7f08a1f95
chwahajahmad <chwahajahmad@gmail.com>
2023-02-27 11:47:42 +0500

    basic errors resolved

--- models/chatModel.js
+++ models/chatModel.js
@@ -5,28 +5,23 @@ const chatSchema = new mongoose.Schema(
     sender: {
       type: mongoose.Types.ObjectId,
       ref: "User",
-      required:true
+      required: true
     },
     receiver: {
       type: mongoose.Types.ObjectId,
       ref: "User",
-      required:true
+      required: true
     },
     message: {
       type: String,
-      required:true
+      required: true
     },
     file: {
-      type: String,
+      type: String
     },
     time: {
-      type: String,
-    },
-    transaction: {
-      type: mongoose.Types.ObjectId,
-      ref: "Transaction",
-      required:true
-    },
+      type: String
+    }
   },
   { timestamps: true }
 );
